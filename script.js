bb633ee39e52731df7eb2003f6bc9b3ef9ebe52b
ThePsience <landon.frailey@icloud.com>
2024-11-11 13:39:55 -0700

    Retry

--- script.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById('button');
-const output = document.getElementById('output');
-
-let count = 0;
-
-button.addEventListener('click', () => {
-  count += 1;
-  output.textContent = count;
-});
